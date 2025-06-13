347dcd752a7b4d2a097da3fd7ed4274573934122
s30679 <s30679@pjwstk.edu.pl>
2025-06-13 13:50:53 +0200

    last

--- Kolokwium2/Services/ExhibitionService.cs
+++ Kolokwium2/Services/ExhibitionService.cs
@@ -20,47 +20,24 @@ public class ExhibitionService : IExhibitionService
 
     public async Task<ExhibitionDetailsDTO?> GetExhibitionsDetailsAsync(int galleryId)
     {
-        var exhebition=await _context.Exhibitions.Include(p => p.GalleryId)
-            .ThenInclude(pr => pr.Doctor)
-            .Include(p => p.Prescriptions)
-            .ThenInclude(pr => pr.PrescriptionMedicaments)
-            .ThenInclude(pm => pm.Medicament).Where(p => p.IdPatient == patientId)
-            .FirstOrDefaultAsync();
+        var exhebition=await _context.Exhibition_Artworks.Include(p => p.Exhibitions)
+            .ThenInclude(pr => pr.ExhibitionId).FirstOrDefaultAsync();
         if(exhebition==null)
         {
             return null;
         }
-        var exhebitionDTO=new ExhibitionDetailsDTO
+
+        var exhebitionDTO = new ExhibitionDetailsDTO
         {
             galleryId = exhebition.GalleryId,
             name = exhebition.Name,
             extablishedDate = exhebition.ExtablishedDate,
-            Artworks = exhebition.Galleries.OrderBy(g => g.Name).ThenBy(g => g.Extension).Select(pr => new PrescriptionDetailsDTO
-            
-        }
-            IdPatient=gallery.IdPatient, 
-            FirstName=gallery.FirstName,
-            LastName=gallery.LastName,
-            Birthdate=gallery.Birthdate,
-            Prescriptions=gallery.Prescriptions.OrderByDescending(p => p.DueDate).Select(pr => new PrescriptionDetailsDTO
+            Artworks = exhebition.Galleries.OrderBy(g => g.Name).Select(pr => new PrescriptionDetailsDTO
             {
-                IdPrescription=pr.IdPrescription,
-                Date=pr.Date,
-                DueDate=pr.DueDate,
-                Doctor=new DoctorDetailsDTO
-                {
-                    IdDoctor=pr.Doctor.IdDoctor,
-                    FirstName=pr.Doctor.FirstName,
-                    LastName=pr.Doctor.LastName
-                },
-                Medicaments=pr.PrescriptionMedicaments.Select(pm => new MedicamentDetailsDTO
-                {
-                    IdMedicament=pm.Medicament.IdMedicament,
-                    Name=pm.Medicament.Name,
-                    Dose=pm.Dose,
-                    Details=pm.Details }).ToList()
+                
             }).ToList()
+            
         };
-        return pacjentDTO;
+        return exhebitionDTO;
     }
 }
